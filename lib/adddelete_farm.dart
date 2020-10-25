5b4d37179b1dd90f09ebdb2a759acb50ec41e82b
mukuro557 <56430402+mukuro557@users.noreply.github.com>
2020-10-26 03:44:03 +0700

    card

--- lib/adddelete_farm.dart
+++ lib/adddelete_farm.dart
@@ -7,7 +7,22 @@ class Add_delete extends StatefulWidget {
 }
 
 class _Add_deleteState extends State<Add_delete> {
-  var farmnum = [{'img':'http://img.painaidii.com/images/20170425_8176_1493102751_242437.jpg','farmname':'สวนยายดา-เจ๊บุญชื่น','address':'เลขที่ 30 เขายายดา ตำบลตะพง อำเภอเมือง ระยอง 21000','price':'30000'}];
+  var farmnum = [
+    {
+      'img':
+          'http://img.painaidii.com/images/20170425_8176_1493102751_242437.jpg',
+      'farmname': 'สวนยายดา-เจ๊บุญชื่น',
+      'address': 'เลขที่ 30 เขายายดา ตำบลตะพง อำเภอเมือง ระยอง 21000',
+      'price': '30000'
+    },
+    {
+      'img':
+          'https://www.technologychaoban.com/wp-content/uploads/2018/12/zx001.jpg',
+      'farmname': 'สวนยายดา-เจ๊บุญชื่น',
+      'address': 'เลขที่ 30 เขายายดา ตำบลตะพง อำเภอเมือง ระยอง 21000',
+      'price': '30000'
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,61 +33,120 @@ class _Add_deleteState extends State<Add_delete> {
       body: Column(
         children: [
           Text('เพิ่มหรือลบสินค้า'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              RaisedButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18)),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Auction()),
-                  );
-                },
-                child: Text('ทั้งหมด'),
-              ),
-              RaisedButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18)),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Auction()),
-                  );
-                },
-                child: Text('กำลังประมูล'),
-              ),
-              RaisedButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18)),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Auction()),
-                  );
-                },
-                child: Text('รอชำระ'),
-              ),
-              RaisedButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18)),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Auction()),
-                  );
-                },
-                child: Text('ชำระแล้ว'),
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.fromLTRB(5, 15, 5, 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18)),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Auction()),
+                    );
+                  },
+                  child: Text('ทั้งหมด'),
+                ),
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18)),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Auction()),
+                    );
+                  },
+                  child: Text('กำลังประมูล'),
+                ),
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18)),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Auction()),
+                    );
+                  },
+                  child: Text('รอชำระ'),
+                ),
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18)),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Auction()),
+                    );
+                  },
+                  child: Text('ชำระแล้ว'),
+                ),
+              ],
+            ),
           ),
           new Expanded(
               child: new ListView.builder(
             itemCount: farmnum.length,
-            itemBuilder: (BuildContext ctxt, int Index) {
-              return new Card(
-                child: Row(children: [Image.network(farmnum[Index]['img']),Column(),],),
+            itemBuilder: (BuildContext ctxt, int index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Card(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 5, 10),
+                        child: Container(
+                            child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image.network(
+                            farmnum[index]['img'],
+                            width: 100,
+                          ),
+                        )),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              farmnum[index]['farmname'],
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15),
+                            ),
+                            Text(
+                              farmnum[index]['address'],
+                              style: TextStyle(fontSize: 9),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 120),
+                              child: Text(
+                                'ราคา ' + farmnum[index]['price'] + ' บาท',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 15),
+                              ),
+                            ),
+                            Padding(
+                                padding: const EdgeInsets.only(left: 150),
+                                child: RaisedButton(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                  ),
+                                  onPressed: () {},
+                                  child: Text(
+                                    'ชำระเงิน',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  color: Color.fromARGB(255, 87, 204, 153),
+                                )),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               );
             },
           ))
