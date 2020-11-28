7ad891721df6c6c267c0ea1627dbc5f01f7ac6b7
mukuro557 <56430402+mukuro557@users.noreply.github.com>
2020-11-28 17:12:44 +0700

    add newpge

--- lib/main.dart
+++ lib/main.dart
@@ -5,27 +5,32 @@ import 'package:fungarden/addproduct.dart';
 import 'package:fungarden/auction.dart';
 import 'package:fungarden/auctionpro.dart';
 import 'package:fungarden/history.dart';
+import 'package:fungarden/login.dart';
 import 'package:fungarden/money.dart';
 import 'package:fungarden/newmain.dart';
+import 'package:fungarden/owner.dart';
 import 'package:fungarden/productstatus.dart';
 import 'package:fungarden/sellhistory.dart';
 import 'package:fungarden/statusCus.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/auction',
+    initialRoute: '/login',
     routes: {
       '/add_delete': (context) => Adddelete(),
+      
       '/add_product': (context) => AddProduct(),
       '/add_pro': (context) => AddP(),
       '/auction': (context) => Auction(),
       '/auction_pro': (context) => AuctionProduct(),
+      '/owner': (context) => Owner(),
       '/history': (context) => History(),
       '/money': (context) => Money(),
       '/newmain': (context) => NewmainPage(),
       '/producstatus': (context) => ProductSta(),
       '/sellhistory': (context) => Sellhistory(),
       '/statusCus': (context) => StatusCus(),
+      '/login': (context) => Login(),
     },
   ));
 }
