fc33cafd850c6eed83832a48124b4d8fe40d68d6
mukuro557 <56430402+mukuro557@users.noreply.github.com>
2020-12-05 23:34:34 +0700

    แก้หน้าแรก

--- lib/newmain.dart
+++ lib/newmain.dart
@@ -2,7 +2,8 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:fungarden/firstpage.dart';
+
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fungarden/auth.dart';
 
@@ -14,7 +15,7 @@ class NewmainPage extends StatefulWidget {
 class _NewmainPageState extends State<NewmainPage> {
   int _gValue = 0;
   String name = '';
-  String _url = 'http://10.0.2.2:35000/';
+
   void changeRadio(int value) {
     setState(() {
       _gValue = value;
@@ -48,13 +49,7 @@ class _NewmainPageState extends State<NewmainPage> {
     Navigator.pushNamedAndRemoveUntil(context, "/login", (route) => false);
   }
 
-  final List fruit = [
-    {
-      'name': 'สวนเงาะลุงจ่า',
-      'location': '54 ม.3 ต.รอบเวียง อ.เมือง จ.เชียงราย',
-      'image': 'rambutan.jpg'
-    },
-  ];
+
 
   _userinfo() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -71,58 +66,13 @@ class _NewmainPageState extends State<NewmainPage> {
     }
   }
 
-  _getallfriut() async {
-    try {
-      http.Response response = await http
-          .get(
-            _url + 'allfruit',
-          )
-          .timeout(Duration(seconds: 5));
-      var res = jsonDecode(response.body);
-      if (response.statusCode == 200) {
-        double county = res[0].length / 6;
-        int count = county.toInt();
 
-        for (int i = 0; i < count; i++) {
-          String idsen = (res[i]['Farm_id']).toString();
-          print(idsen);
-          try {
-            http.Response responses = await http.post(_url + 'allfruit',
-                body: {"id": idsen}).timeout(Duration(seconds: 5));
-            var respon = responses.body;
-
-            setState(() {
-              var reson = jsonDecode(respon);
-
-              fruit.add(
-                {
-                  'name': reson[0]['Farm_name'],
-                  'location': reson[0]['Address'],
-                  'image': 'rambutan.jpg'
-                },
-              );
-            });
-          } on TimeoutException catch (e) {
-            print('Timeout: $e');
-          } catch (e) {
-            print('Error: $e');
-          }
-        }
-      } else {
-        showAlert(response.body.toString());
-      }
-    } on TimeoutException catch (e) {
-      print('Timeout: $e');
-    } catch (e) {
-      print('Error: $e');
-    }
-  }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _getallfriut();
+    
     _userinfo();
   }
 
@@ -159,173 +109,7 @@ class _NewmainPageState extends State<NewmainPage> {
           ),
         ),
         body: TabBarView(children: [
-          Container(
-            color: Colors.grey[200],
-            child: Column(
-              children: [
-                SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Row(
-                    children: [
-                      Column(
-                        children: [
-                          MaterialButton(
-                            onPressed: () {},
-                            color: Colors.red[300],
-                            textColor: Colors.white,
-                            child: Image.asset(
-                              'assets/images/hot.png',
-                              width: 18,
-                            ),
-                            padding: EdgeInsets.all(14),
-                            shape: CircleBorder(),
-                          ),
-                          Text('มาแรง')
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          MaterialButton(
-                            onPressed: () {},
-                            color: Colors.yellow[300],
-                            textColor: Colors.white,
-                            child: Icon(
-                              Icons.pin_drop,
-                              size: 20,
-                              color: Colors.black,
-                            ),
-                            padding: EdgeInsets.all(14),
-                            shape: CircleBorder(),
-                          ),
-                          Text('ใกล้สุด')
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          MaterialButton(
-                            onPressed: () {},
-                            color: Colors.orangeAccent,
-                            textColor: Colors.white,
-                            child: Image.asset(
-                              'assets/images/sale.png',
-                              width: 20,
-                            ),
-                            padding: EdgeInsets.all(14),
-                            shape: CircleBorder(),
-                          ),
-                          Text('ราคาดี')
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          MaterialButton(
-                            onPressed: () {},
-                            color: Color.fromRGBO(56, 163, 165, 10),
-                            textColor: Colors.white,
-                            child: Image.asset(
-                              'assets/images/premium.png',
-                              width: 18,
-                            ),
-                            padding: EdgeInsets.all(14),
-                            shape: CircleBorder(),
-                          ),
-                          Text('พรีเมี่ยม')
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(9.0),
-                  child: Row(
-                    children: [
-                      Text(
-                        'ล่าสุด',
-                        style: TextStyle(fontSize: 28),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: fruit.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.pushNamed(context, "/auction");
-                          },
-                          child: Card(
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(9.0),
-                                  child: Container(
-                                    child: Image.asset(
-                                      'assets/images/' + fruit[index]['image'],
-                                      height: 200,
-                                    ),
-                                  ),
-                                ),
-                                Column(
-                                  children: [
-                                    Text(fruit[index]['name']),
-                                    Text('${fruit[index]['location']} '),
-                                  ],
-                                ),
-                                SizedBox(height: 5),
-                                Column(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Row(
-                                        children: [
-                                          Icon(
-                                            Icons.alarm,
-                                            size: 20,
-                                          ),
-                                          SizedBox(width: 5),
-                                          Text('00:59:34'),
-                                        ],
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Row(
-                                        children: [
-                                          Image.asset(
-                                            'assets/images/auction.png',
-                                            width: 20,
-                                          ),
-                                          SizedBox(width: 5),
-                                          Text('5000 บาท'),
-                                          Spacer(),
-                                          Icon(Icons.people),
-                                          Text('10 คน')
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                )
-              ],
-            ),
-          ),
+          Firstpage(),
           Container(
             color: Colors.grey[200],
             child: Column(
@@ -376,7 +160,7 @@ class _NewmainPageState extends State<NewmainPage> {
                                   child: Column(
                                     children: [
                                       Text(
-                                        'สวนเงาะลุงจ่า',
+                                        'สเงาะลุงจ่า',
                                         style: TextStyle(fontSize: 22),
                                       ),
                                       Text(
